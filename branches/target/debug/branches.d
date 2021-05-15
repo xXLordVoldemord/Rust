4a787cd356f910c3ed3ff39da7c0146327a67be0
Lars-Manuel Schneider <lars-manuelschneider@Mac-mini-von-Lars-Manuel.local>
2021-05-15 19:31:09 +0200

    if branches

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/lars-manuelschneider/Documents/Heleos_dev/rust_trial/branches/target/debug/branches: /Users/lars-manuelschneider/Documents/Heleos_dev/rust_trial/branches/src/main.rs
---
+++ branches/target/debug/deps/branches.d
@@ -0,0 +1,5 @@
+/Users/lars-manuelschneider/Documents/Heleos_dev/rust_trial/branches/target/debug/deps/branches: src/main.rs
+
+/Users/lars-manuelschneider/Documents/Heleos_dev/rust_trial/branches/target/debug/deps/branches.d: src/main.rs
+
+src/main.rs:
